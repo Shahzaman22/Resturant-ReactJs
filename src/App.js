858d58b541ab3c7ecd4cfd24ae6a11f807e26a52
[Shahzaman] <shahzaman.aftab@gmail.com>
2022-09-08 22:07:23 +0500

    All fixes are solved

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+//import UseReducer from "./component/Hooks/UseReducer";
+import Resturant from "./component/Basics/Resturant";
+//import UseState from "./component/Hooks/UseState";
+// import UseEffect from "./component/Hooks/UseEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+// import Todo from "./component/todoreact/todo";
+//import Temp from "./component/weather/temp";
+
+const App = () => {
+  return ( 
+    <Resturant/>
+   //<UseState/>
+  //  <UseEffect/>
+  // <UseReducer/>
+  //  <Todo/>
+    //  <Temp/>
+  
+  );
+};
+
+export default App;
